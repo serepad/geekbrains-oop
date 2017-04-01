211e5120f505ba6ef289cae719b9ba9cdd63c031
Sergey Padzhev <serhio_j@mail.ru>
2017-04-01 18:08:42 +0300

    Класс List

--- snake/snake/snake/Program.cs
+++ snake/snake/snake/Program.cs
@@ -11,10 +11,18 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p3 = new Point(5, 6, '@');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
 
             Console.ReadLine();
 
